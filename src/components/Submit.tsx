2b6f5e69d4016c75523daf6bc10c52fdd05d28ab
Nayeon Keum <rmaskdus0208@gmail.com>
2023-06-04 04:52:41 +0900

    [feat/#26] Add: connect Submit with BE&Infra

--- src/components/Submit.tsx
+++ src/components/Submit.tsx
@@ -1,8 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
+import axios from "axios";
 import SubmitterList from "../props/SubmitterList";
 import "../style/home.css";
 import Navbar from "./Navbar";
 const Submit: FC = () => {
+  // data interface(now deprecated) TODO: apply
+  interface UrlResp {
+    id: string;
+    lectureId: string;
+    containerAddress: string;
+    apiEndpoint: string;
+  }
+
+  // 제출 페이지와 연동
+  // const location = useLocation();
+  // const assignment_id = location.state.assignment_id;
+  const assignment_id = "f58b21b6-0a6e-4cad-a563-77d370ef067e";
+
+  const [data, setData] = useState<UrlResp[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://moaroom-back.duckdns.org:8080/assignment/list/" + assignment_id
+      )
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
+
   return (
     <>
       <div className="background">
@@ -30,7 +56,9 @@ const Submit: FC = () => {
               일괄 채점
             </button>
           </div>
-          <SubmitterList />
+          {data.map((user) => (
+            <SubmitterList user={user} />
+          ))}
         </div>
       </div>
     </>

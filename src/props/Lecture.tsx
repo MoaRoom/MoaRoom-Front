0018406b60eec250339ee94f5b36ac09f3062d51
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-06-04 04:34:14 +0900

    [feat/#23] Add: LectureListPage connect Back

--- src/props/Lecture.tsx
+++ src/props/Lecture.tsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { LectureType } from "./LectureList";
+import axios from "axios";
+import {useLocation} from "react-router-dom";
+import { useNavigate } from "react-router";
+import { LectureType } from "../components/LecturePage";
 
 type LectureProps = {
   // 부모 컴포넌트에 import 해온 타입을 재사용
   lecture: LectureType;
 };
 const Lecture = ({ lecture }: LectureProps) => {
-  const { title, professor_name, room, enroll } = lecture;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const user_id = location.state.user_id;
+  const { lecture_id, title, professor_name, room, enroll } = lecture;
+  const Enroll = (data: any) => {
+    let params = {
+      lecture_id: lecture_id,
+      student_id: user_id,
+    };
+    console.log(params);
+
+    // TODO 서버 나오면 디버깅 필요
+    axios
+      .post("http://moaroom-back.duckdns.org:8080/lecture/enroll", params)
+      .then(function(response) {
+        console.log(response)
+        if (response.data === "강의 신청 완료") {
+          alert("강의 신청이 완료 되었습니다.")
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
   return (
     <div className="card mb-2 mt-2 rounded">
           <div className="card-body">
@@ -23,7 +49,7 @@ const Lecture = ({ lecture }: LectureProps) => {
             <button className="btn btn-outline-success text-center shadow-none mb-3" style={{visibility:"hidden",width: "200px", height:"55px"}}>강의 신청 하기</button>
             )}
           {enroll == false && (
-            <button className="btn btn-outline-success text-center shadow-none mb-3" style={{width: "200px", height:"55px"}}>강의 신청 하기</button>
+            <button className="btn btn-outline-success text-center shadow-none mb-3" style={{width: "200px", height:"55px"}} onClick={Enroll}>강의 신청 하기</button>
           )}
         </div>
       </div>

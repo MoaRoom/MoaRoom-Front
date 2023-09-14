d4124cb302ce56595bde8c1f0b16474601e16673
Nayeon Keum <nkeum@lablup.com>
2023-09-14 23:35:02 +0900

    [feat/#72] Add: merge develop

--- src/components/Submit.tsx
+++ src/components/Submit.tsx
@@ -26,23 +26,19 @@ const Submit: FC = () => {
   useEffect(() => {
     api.client.get("/steps/" + assignment_id).then((response) => {
       if (isProfessor) {
-        console.log(response.data)
-        // 기존 코드로 인해 list에 값이 중복으로 들어가게 되므로 삭제
-        //for (let i = 0; i < response.data.length; i++) {
-        //   tmpList.push({
-        //     id: response.data[i].id,
-        //     name: response.data[i].name,
-        //     step: response.data[i].step,
-        //     score: response.data[i].score,
-        //     user_id: user_id,
-        //     assignment_id: assignment_id,
-        //     lecture_id: lecture_id,
-        //     isProfessor: isProfessor,
-        //   } as SubmitterPropType);
-        //   console.log(tmpList)
-        // }
-        // setSubmittersPropsList(tmpList);
-        setSubmittersPropsList(response.data)
+        for (let i = 0; i < response.data.length; i++) {
+          tmpList.push({
+            id: response.data[i].id,
+            name: response.data[i].name,
+            step: response.data[i].step,
+            score: response.data[i].score,
+            user_id: user_id,
+            assignment_id: assignment_id,
+            lecture_id: lecture_id,
+            isProfessor: isProfessor,
+          } as SubmitterPropType);
+        }
+        setSubmittersPropsList(tmpList);
       } else {
         // 학생 것만 보이게
         for (let i = 0; i < response.data.length; i++) {
@@ -58,13 +54,12 @@ const Submit: FC = () => {
             } as SubmitterPropType);
           }
         }
-        console.log(submittersPropsList)
+        setSubmittersPropsList(tmpList);
       }
     });
   }, []);
 
   const autoScore = () => {
-    console.log(submittersPropsList)
     // user_id is professor
     console.log("/users/" + user_id + "/urls");
     api.client.get("/users/" + user_id + "/urls").then((response) => {
@@ -72,28 +67,27 @@ const Submit: FC = () => {
       response.data.map((urlmodel: any) => {
         const apiEP = urlmodel.apiEndpoint;
         for (let i = 0; i < submittersPropsList.length; i++) {
+          var student_id = submittersPropsList[i].id;
           axios
             .get(
               apiEP +
                 "/assignment/?id=" +
-                submittersPropsList[i].id +
+                student_id +
                 "&assignment_id=" +
                 assignment_id
             )
             .then((response) => {
-              console.log(response)
               var answer = JSON.parse(response.data).answer;
               var runtime = JSON.parse(response.data).runtime;
               let params = {
-                user_id: submittersPropsList[i].id,
+                user_id: student_id,
                 answer: answer.slice(0, -1), // infra comes with /n
                 runtime: 1.0,
               };
-              console.log(params)
               api.client
                 .post("/assignments/" + assignment_id + "/auto", params)
                 .then(() => {
-                  console.log("자동채점 api 호출")
+                  window.location.reload();
                 });
             });
         }
